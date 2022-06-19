b6fa48fbe348d1c22265edefe2187014ff426c1c
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-19 06:58:44 +0000

    update

--- whatsap_clone/lib/main.dart
+++ whatsap_clone/lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Whatsapp clone',
+      title: 'WhatsApp',
       navigatorKey: StackedService.navigatorKey,
       onGenerateRoute: StackedRouter().onGenerateRoute,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
     );
   }
