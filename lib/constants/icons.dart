1ee4c7976932ce5772bbfa8fedbb73e8a7478f8d
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-22 16:44:07 +0000

    update

---
+++ lib/constants/icons.dart
@@ -0,0 +1,9 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const kStatusIcon = FontAwesomeIcons.circleNotch;
+const kCallIcon = FontAwesomeIcons.phone;
+const kCameraIcon = FontAwesomeIcons.camera;
+const kSettingsIcon = FontAwesomeIcons.gear;
+const kChatsIcon = FontAwesomeIcons.comments;
+const kDropdownIcon = FontAwesomeIcons.angleDown;
+const kEditNoteIcon = FontAwesomeIcons.penToSquare;
