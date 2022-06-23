4dbb7d4f47708e800a4abfcecf037954d0090fe3
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-23 13:06:45 +0000

    update

--- lib/screens/mobile/bottom_nav_bar.dart
+++ lib/screens/mobile/bottom_nav_bar.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:whatsap_clone/constants/icons.dart';
-import 'package:whatsap_clone/screens/mobile/mobile_chats_screen.dart';
+import 'package:whatsap_clone/constants/themes.dart';
+import 'package:whatsap_clone/screens/mobile/chats_screen.dart';
+import 'package:whatsap_clone/screens/mobile/settings.dart';
+import 'chats_screen.dart';
 
-import 'mobile_chats_screen.dart';
-
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({Key? key}) : super(key: key);
 
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
     List<Widget> screens = [
       const MobileChatsScreen(),
+      const MobileSettings(),
     ];
-    int currentScreenIndex = 0;
-    void changedScreen(int nextScreenIndex) {
-      currentScreenIndex = nextScreenIndex;
-      print(currentScreenIndex);
-    }
+    int currentScreenIndex = 1;
 
     return Scaffold(
+      backgroundColor: kBackgroundColor,
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: currentScreenIndex,
         selectedLabelStyle: const TextStyle(fontSize: 12),
         unselectedLabelStyle: const TextStyle(fontSize: 12),
         selectedItemColor: Colors.blue,
@@ -28,7 +34,7 @@ class BottomNavBar extends StatelessWidget {
         showUnselectedLabels: true,
         iconSize: 20,
         unselectedItemColor: Colors.grey,
-        onTap: (value) => changedScreen(value),
+        onTap: (value) => setState(() => currentScreenIndex = value),
         items: const [
           BottomNavigationBarItem(
             label: 'Status',
@@ -55,7 +61,9 @@ class BottomNavBar extends StatelessWidget {
           ),
         ],
       ),
-      body: screens[currentScreenIndex],
+      body: SafeArea(
+        child: screens[currentScreenIndex],
+      ),
     );
   }
 }
