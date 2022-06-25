81ad2f9da185f888e266a1daafd225b9f2165197
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-25 02:08:59 +0000

    update

--- lib/widgets/chat_item.dart
+++ lib/widgets/chat_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:whatsap_clone/constants/icons.dart';
@@ -13,7 +14,11 @@ class ChatItem extends StatelessWidget {
       child: Row(children: [
         Row(
           children: [
-            const CircleAvatar(),
+            const Material(
+              child: CircleAvatar(
+                radius: 26,
+              ),
+            ),
             const SizedBox(
               width: 8,
             ),
@@ -24,8 +29,10 @@ class ChatItem extends StatelessWidget {
                   'Samuel',
                   style: TextStyle(),
                 ),
-                Text('How are you doing ?',
-                    style: TextStyle(color: Colors.grey))
+                Text(
+                  'How are you doing ?',
+                  style: TextStyle(color: CupertinoColors.systemGrey),
+                )
               ],
             )
           ],
@@ -36,9 +43,29 @@ class ChatItem extends StatelessWidget {
             const Text('29/08/22'),
             Row(
               children: [
-                const Chip(
-                  label: Text('221'),
+                const Material(
+                  child: Chip(
+                    label: Text('322'),
+                  ),
                 ),
+                // Column(
+                //   mainAxisSize: MainAxisSize.min,
+                //   children: [
+                //     Container(
+                //       padding: const EdgeInsets.symmetric(
+                //         horizontal: 20,
+                //         vertical: 3,
+                //       ),
+                //       decoration: const BoxDecoration(
+                //         color: CupertinoColors.systemGreen,
+                //         shape: BoxShape.circle,
+                //       ),
+                //       child: const Center(
+                //         child: Text('221'),
+                //       ),
+                //     ),
+                //   ],
+                // ),
                 if (onHover ?? false) const FaIcon(kDropdownIcon)
               ],
             ),
