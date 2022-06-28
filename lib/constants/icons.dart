b9e6e7f2f08532f360d8cbc3e79538b814aba7c0
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-06-28 13:02:21 +0000

    update

--- lib/constants/icons.dart
+++ lib/constants/icons.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const kStatusIcon = FontAwesomeIcons.circleNotch;
 const kCallIcon = FontAwesomeIcons.phone;
 const kCameraIcon = FontAwesomeIcons.camera;
 const kSettingsIcon = FontAwesomeIcons.gear;
-const kChatsIcon = FontAwesomeIcons.comments;
+const kChatsIcon = CupertinoIcons.chat_bubble_2;
 const kDropdownIcon = FontAwesomeIcons.angleDown;
 const kEditNoteIcon = FontAwesomeIcons.penToSquare;
 const kQRcodeIcon = FontAwesomeIcons.qrcode;
@@ -14,3 +15,4 @@ const kLaptopIcon = FontAwesomeIcons.laptop;
 const kKeyIcon = FontAwesomeIcons.key;
 const kWhatsapIcon = FontAwesomeIcons.whatsapp;
 const kinfoIcon = FontAwesomeIcons.info;
+const kPhonePlus = CupertinoIcons.phone_badge_plus;
