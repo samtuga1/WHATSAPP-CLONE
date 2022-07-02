2c41fea960a3d5844caf9248a34624ce425c30ea
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-02 00:34:54 +0000

    update

--- lib/providers/mobile/about_provider.dart
+++ lib/providers/mobile/about_provider.dart
@@ -1,11 +1,29 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 
 class AboutScreenProvider extends ChangeNotifier {
   String _currentStatusOption = 'Available';
   String get currentStatusOption => _currentStatusOption;
+  bool _loading = false;
+  bool get loading => _loading;
+
+  void fakeDelay(selectedStatus) {
+    _loading = true;
+    log(_currentStatusOption.toString());
+    notifyListeners();
+    Timer(const Duration(seconds: 2), () {
+      _loading = false;
+      changeStatus(selectedStatus);
+      log(_currentStatusOption.toString());
+      notifyListeners();
+    });
+  }
 
   void changeStatus(String selectedStatus) {
     _currentStatusOption = selectedStatus;
+    notifyListeners();
   }
 
   List<String> statusOptions = [
@@ -18,6 +36,6 @@ class AboutScreenProvider extends ChangeNotifier {
     'In a meeting',
     'At the gym',
     'Sleeping',
-    'Urgent calls only,'
+    'Urgent calls only'
   ];
 }
