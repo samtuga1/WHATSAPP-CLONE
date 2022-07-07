8f0c1ff62cb2ae4b7e0a98d82fbd30c07e2bc1f7
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-07 01:07:11 +0000

    update

--- lib/screens/mobile/story_screen.dart
+++ lib/screens/mobile/story_screen.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 import 'dart:developer';
 import 'package:dismissible_page/dismissible_page.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:whatsap_clone/constants/themes.dart';
 
 class StoryScreen extends StatefulWidget {
   static const routeName = '/story_screen';
@@ -55,6 +57,7 @@ class _StoryScreenState extends State<StoryScreen> {
   }
 
   void onTapDown(TapDownDetails ontapDown) {
+    swipedUp = false;
     final screenWidth = MediaQuery.of(context).size.width;
     double dx = ontapDown.globalPosition.dx;
     if (mounted) {
@@ -82,9 +85,11 @@ class _StoryScreenState extends State<StoryScreen> {
     }
   }
 
-  double backgroundOpacity = 0;
+  void swipeUp(TapDownDetails ontapDown) {}
 
+  double backgroundOpacity = 0;
   List<double> percentCompletedList = [];
+  bool swipedUp = false;
 
   @override
   void initState() {
@@ -105,119 +110,248 @@ class _StoryScreenState extends State<StoryScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      body: GestureDetector(
-        onTapDown: (details) => onTapDown(details),
-        child: DismissiblePage(
-          backgroundColor: Colors.black.withOpacity(backgroundOpacity),
-          onDragUpdate: (details) {
-            backgroundOpacity = 1;
-            if (details.overallDragValue == 0) {
-              backgroundOpacity = 0;
+      body: DismissiblePage(
+        backgroundColor: Colors.black.withOpacity(backgroundOpacity),
+        onDragUpdate: (DismissiblePageDragUpdateDetails details) {
+          backgroundOpacity = 1;
+          if (details.overallDragValue == 0) {
+            backgroundOpacity = 0;
+          }
+        },
+        maxRadius: 1000,
+        minRadius: 0,
+        onDismissed: () {
+          Navigator.of(context).pop();
+        },
+        direction: DismissiblePageDismissDirection.down,
+        maxTransformValue: 1,
+        isFullScreen: true,
+        child: GestureDetector(
+          onTapDown: (details) => onTapDown(details),
+          onPanStart: (details) {
+            if (details.globalPosition.direction > 1) {
+              swipedUp = true;
             }
           },
-          maxRadius: 1000,
-          minRadius: 0,
-          onDismissed: () {
-            Navigator.of(context).pop();
-          },
-          direction: DismissiblePageDismissDirection.multi,
-          isFullScreen: false,
           child: Hero(
-              tag: 'story_screen',
-              child: Stack(
-                children: [
-                  pages[currentPageIndex],
-                  SafeArea(
-                    child: Container(
-                      margin: const EdgeInsets.only(top: 1, bottom: 0),
-                      child: Row(
-                        children: List.generate(
-                          pages.length,
-                          (index) => Expanded(
-                            child: LinearPercentIndicator(
-                              lineHeight: 4,
-                              backgroundColor: Colors.grey.shade300,
-                              barRadius: const Radius.circular(3),
-                              percent: percentCompletedList[index],
-                              progressColor: Colors.white,
+            tag: 'story_screen',
+            child: Stack(
+              children: [
+                Stack(
+                  children: [
+                    pages[currentPageIndex],
+                    SafeArea(
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 1, bottom: 0),
+                        child: Row(
+                          children: List.generate(
+                            pages.length,
+                            (index) => Expanded(
+                              child: LinearPercentIndicator(
+                                lineHeight: 4,
+                                backgroundColor: Colors.grey.shade300,
+                                barRadius: const Radius.circular(3),
+                                percent: percentCompletedList[index],
+                                progressColor: Colors.white,
+                              ),
                             ),
-                          ),
-                        ).toList(),
+                          ).toList(),
+                        ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    top: 45,
-                    child: Row(
-                      children: [
-                        Material(
-                          color: Colors.transparent,
-                          child: IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.arrow_back_ios_new,
+                    Positioned(
+                      top: 45,
+                      child: Row(
+                        children: [
+                          Material(
+                            color: Colors.transparent,
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.arrow_back_ios_new,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          const CircleAvatar(),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Samuel',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18.5,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                '8:43 AM',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      top: 48,
+                      right: 0,
+                      child: Material(
+                        color: Colors.transparent,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.more_horiz,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 20),
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          children: const [
+                            Icon(
+                              CupertinoIcons.arrow_up,
                               color: Colors.white,
                             ),
-                          ),
-                        ),
-                        const CircleAvatar(),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
                             Text(
-                              'Samuel',
+                              'Reply',
                               style: TextStyle(
                                 color: Colors.white,
-                                fontSize: 18.5,
-                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
                               ),
-                            ),
-                            Text(
-                              '8:43 AM',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
+                            )
                           ],
                         ),
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    top: 48,
-                    right: 0,
-                    child: Material(
-                      color: Colors.transparent,
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.more_horiz,
-                          color: Colors.white,
-                        ),
                       ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: GestureDetector(
-                      onVerticalDragUpdate: (details) {
-                        print(details.globalPosition.dy);
-                      },
-                      child: const Icon(
-                        CupertinoIcons.arrow_up,
-                        color: Colors.white,
-                      ),
-                    ),
-                  )
-                ],
-              )),
+                    )
+                  ],
+                ),
+                if (swipedUp) StatusMessageBox(swipedUp: swipedUp)
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class StatusMessageBox extends StatelessWidget {
+  const StatusMessageBox({
+    Key? key,
+    required this.swipedUp,
+  }) : super(key: key);
+
+  final bool swipedUp;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          color: Colors.grey.shade300,
+          height: 52,
+          width: double.infinity,
+          child: Row(children: [
+            Container(
+              margin: const EdgeInsets.only(left: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Samuel • Status',
+                    style: TextStyle(color: Colors.purpleAccent),
+                  ),
+                  Row(
+                    children: const [
+                      Icon(
+                        CupertinoIcons.camera_fill,
+                        size: 18,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        'Photo',
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const Spacer(),
+            Container(
+              margin: const EdgeInsets.only(right: 5),
+              height: 44,
+              width: 44,
+              decoration: BoxDecoration(
+                color: Colors.pinkAccent,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            )
+          ]),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          height: 45,
+          width: double.infinity,
+          color: kBackgroundColor,
+          child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 6),
+                child: const Icon(CupertinoIcons.add),
+              ),
+              Expanded(
+                child: TextField(
+                  autofocus: swipedUp,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 6),
+                child: const Icon(CupertinoIcons.camera),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 6),
+                child: const Icon(CupertinoIcons.mic),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
