a6b3d92aed7068a20f92050d9ac195050a6c7706
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-07 23:15:49 +0000

    Initialized Firebase in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:whatsap_clone/firebase_options.dart';
 import 'package:whatsap_clone/providers/mobile/about_provider.dart';
 import 'package:whatsap_clone/screens/home.dart';
 import 'package:whatsap_clone/screens/mobile/about_screen.dart';
 import 'package:whatsap_clone/screens/mobile/profile_screen.dart';
 import 'package:whatsap_clone/screens/mobile/story_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
